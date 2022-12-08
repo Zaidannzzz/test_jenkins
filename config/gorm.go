307b027ca0bfa7fec4a51657cf13f00946846924
Zaidan Zulhakim <59697378+Zaidannzzz@users.noreply.github.com>
2022-12-08 15:12:00 +0700

    First Commit

---
+++ config/gorm.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"zaidanweb/httpserver/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect() (*gorm.DB, error) {
+	dbCredential := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", os.Getenv("PGHOST"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"), os.Getenv("PGPORT"))
+
+	db, err := gorm.Open(postgres.Open(dbCredential), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&models.UserModel{})
+	return db, err
+}
